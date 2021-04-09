84f03d19bf8920905e7bd5efdea9eec70feeb9f2
Grisehyrdene <Dmitri.Danshin2017@gmail.com>
2021-04-09 19:17:03 +0300

    structure

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'feb069d659b2bab3d0fd11ea1f7b8602ed368d8d',
+    'name' => 'cms_project/cms',
+  ),
+  'versions' => 
+  array (
+    'cms_project/cms' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'feb069d659b2bab3d0fd11ea1f7b8602ed368d8d',
+    ),
+  ),
+);
